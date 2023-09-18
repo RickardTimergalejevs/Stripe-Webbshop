28eaea5469bdaecbeb039d546148fea243c69579
RickardTimergalejevs <rickard.time@gmail.com>
2023-09-18 17:16:46 +0200

    Confirmation page styling

--- Client/src/components/ConfirmationPage/ConfirmationPage.tsx
+++ Client/src/components/ConfirmationPage/ConfirmationPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useUserContext } from "../../context/UserContext"
 import { formatDate, formatPrice } from "../../utils/helpers"
 import { useOrderContext } from "../../context/OrderContext"
+import "./ConfirmationPage.css"
 
 const ConfirmationPage = () => {
   const { verifyPayment, isPaymentVerified, order } = useOrderContext()
@@ -12,19 +13,21 @@ const ConfirmationPage = () => {
   }, [])
 
   return (
-    <div>
+    <main>
       {isPaymentVerified && user && order && (
-        <div>
-          <h1>Thank you for your order, {user.username}!</h1>
-          <div>
-            <h2>Order details:</h2>
-            <p>Order number: {order.order_id}</p>
-            <p>Created: {formatDate(order.created)}</p>
+        <div className="confirmation-wrapper">
+          <h1 className="confirmation-title">Thank you for your order, {user.username}!</h1>
+          <div className="confirmation-details">
             <div>
+              <h2>Order details:</h2>
+              <p>Order number: {order.order_id}</p>
+              <p>Created: {formatDate(order.created)}</p>
+            </div>
+            <div className="confirmation-order-list">
               {order.products.map((item) => (
                 <div key={item.product_id}>
                   <h2>{item.product}</h2>
-                  <img src={item.image} alt={item.product} />
+                  <img src={item.image} alt={item.product} className="confirmation-product-img"/>
                   <p>Price: {formatPrice(item.total_price)} kr</p>
                   <p>Quantity: {item.quantity}</p>
                 </div>
@@ -34,7 +37,7 @@ const ConfirmationPage = () => {
           </div>
         </div>
       )}
-    </div>
+    </main>
   )
 }
 
